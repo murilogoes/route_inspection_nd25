e058b31f91c4150a96639f97bf7005556ad54b0d
Murilo Góes de Almeida <murilow@gmail.com>
2021-02-25 22:44:51 -0300

    implementando e testando recursao

--- grafo4.py
+++ grafo4.py
@@ -1,6 +1,7 @@
 import secrets
 from copy import deepcopy
 
+
 # a implementacao desse grafo se dará por lista de adjacencias
 # TESTANDO COM FORÇA BRUTA
 
@@ -14,6 +15,7 @@ class Vertice:
         if v not in self.vizinhos:
             self.vizinhos.append((Aresta(v, peso, direcionado, secrets.token_hex(nbytes=16))))
 
+
 class Aresta:
     def __init__(self, v, p, direcionado, hash):
         self.destino = v
@@ -22,7 +24,8 @@ class Aresta:
         self.visitado = False
         self.quantidade_visitas = 0
         self.hash = hash
-        #considerar colocar a origem tambem
+        # considerar colocar a origem tambem
+
 
 class Caminho:
     def __init__(self, percurso):
@@ -65,8 +68,7 @@ class Grafo:
             for j in dd.vizinhos:
                 self.arestas_hashes.append(j.hash)
 
-
-    #imprime o grafo como lista de adjacencias
+    # imprime o grafo como lista de adjacencias
     def imprime_grafo(self):
         for dd in self.vertices.values():
             lista = dd.nome + ":"
@@ -74,7 +76,7 @@ class Grafo:
                 lista = f'{lista} -> {j.destino} ({j.peso})({j.direcionado})'
             print(lista)
 
-    #metodo me retorna uma aresta passando o vertice de origem e o destino
+    # metodo me retorna uma aresta passando o vertice de origem e o destino
     def get_aresta(self, v, d):
         for i in self.vertices[v].vizinhos:
             if i.destino == d:
@@ -91,7 +93,7 @@ class Grafo:
 
         # montando uma lista dos hashes nao percorridos
         hashes_nao_percorridos = list()
-        #for j in arestas_percorridas:
+        # for j in arestas_percorridas:
         for j in self.arestas_hashes:
             if j not in arestas_percorridas_hashes:
                 hashes_nao_percorridos.append(j)
@@ -154,6 +156,12 @@ class Grafo:
     # metodo para avancar ao proximo vertice
     def avanca_proximo(self, v, i, ncaminho):
 
+        if ncaminho > len(self.caminhos):
+            #print("novo caminho")
+            self.caminhos.append(deepcopy(self.caminhos[ncaminho - 2]))
+            # self.caminhos[ncaminho - 1].percurso += v
+            # self.caminhos[ncaminho - 1].visitado = True
+
         contem_aresta = False
         nova_aresta = {}
 
@@ -165,36 +173,38 @@ class Grafo:
                 nova_aresta = j
                 break
         if contem_aresta:
-            nova_aresta.quantidade_visitas +=1
+            nova_aresta.quantidade_visitas += 1
         else:
-        # se for uma aresta nova, eu pego ela e jogo no caminho
+            # se for uma aresta nova, eu pego ela e jogo no caminho
             nova_aresta = deepcopy(i)
             nova_aresta.quantidade_visitas += 1
             nova_aresta.visitado = True
             self.caminhos[ncaminho - 1].arestas_percorridas.append(nova_aresta)
 
         # independente dos dois casos acima, incrementando o percurso e o peso
-        self.caminhos[ncaminho - 1].percurso += i.destino
+        self.caminhos[ncaminho - 1].percurso += "->" + i.destino
         self.caminhos[ncaminho - 1].custo += i.peso
-        print(f'indo de {v} para {i.destino} caminho {ncaminho}')
+        #print(f'indo de {v} para {i.destino} caminho {ncaminho}')
 
         # chamando a funcao que vai percorrer o grafo, passando o vertice destino e a posicao do caminho
         self.percorre_grafo(self.vertices[i.destino].nome, ncaminho)
 
-
     # metodo principal que vai fazer caminhar de um vertice do outro, escolhendo a melhor aresta
     def percorre_grafo(self, v, ncaminho):
 
+        print(f'Estou no vertice {v}')
+
         # esse metodo vai me dizer qual vai ser a melhor aresta a ser seguida atraves do vertice que estou
-        def melhor_aresta(lista_aresta, arestas_percorridas, ncaminho):
+        def melhor_aresta(lista_aresta, arestas_percorridas):
             # armazenando o primeiro valor da lista
             melhor = lista_aresta[0]
+            alternativo = None
             for n in range(len(lista_aresta)):
 
                 # se o melhor atual for visitado e o comparado nao, ja atribui
-                if self.caminho_visitado(melhor, arestas_percorridas) and not self.caminho_visitado(lista_aresta[n], arestas_percorridas):
-                    print("melhor ja foi visitado e valor testado nao")
-                #if melhor.visitado and not lista_aresta[n].visitado:
+                if self.caminho_visitado(melhor, arestas_percorridas) and not self.caminho_visitado(lista_aresta[n],
+                                                                                                    arestas_percorridas):
+                    # print("melhor ja foi visitado e valor testado nao")
                     if lista_aresta[n].direcionado:
                         melhor = lista_aresta[n]
                     else:
@@ -204,49 +214,62 @@ class Grafo:
                             melhor = lista_aresta[n]
 
                 # se ambos nao tiverem sido visitados, compara o de menor peso
-                elif not self.caminho_visitado(melhor, arestas_percorridas) and not self.caminho_visitado(lista_aresta[n], arestas_percorridas):
-                    print("nenhum dos dois foram visitados")
-                    #TO MECHENDO AQUI PARA RESOLVER O PROBLEMA DE RECURSAO
-                    #elif not melhor.visitado and not lista_aresta[n].visitado:
+                elif not self.caminho_visitado(melhor, arestas_percorridas) and not self.caminho_visitado(
+                        lista_aresta[n], arestas_percorridas):
+                    # print("nenhum dos dois foram visitados")
+                    # TO MECHENDO AQUI PARA RESOLVER O PROBLEMA DE RECURSAO
+                    # elif not melhor.visitado and not lista_aresta[n].visitado:
                     if lista_aresta[n].peso < melhor.peso:
-                        # if lista_aresta[n].peso > melhor.peso:
-                        #self.percorre_grafo(melhor.destino, len(self.caminhos) + 1)
+                        # if lista_aresta[n].hash != melhor.hash:
+                        alternativo = deepcopy(melhor)
+                            #self.avanca_proximo(v, melhor, len(self.caminhos) + 1)
                         melhor = lista_aresta[n]
-                    else:
-                        murilo = "eae"
-                        # self.percorre_grafo(lista_aresta[n].destino, len(self.caminhos) + 1)
-
+                    #else:
+                      #  if lista_aresta[n].hash != melhor.hash:
+                       #     alternativo = deepcopy(lista_aresta[n])
+                            # self.avanca_proximo(v, lista_aresta[n], len(self.caminhos) + 1)
+                        #print(f'indo de {v}  para {melhor.destino} caminho {ncaminho}')
+                    # else:
+                    #     self.avanca_proximo(v, lista_aresta[n], len(self.caminhos) + 1)
                 # se ambos ja foram visitados
-                #else:
-                elif self.caminho_visitado(melhor, arestas_percorridas) and self.caminho_visitado(lista_aresta[n], arestas_percorridas):
+                # else:
+                elif self.caminho_visitado(melhor, arestas_percorridas) and self.caminho_visitado(lista_aresta[n],
+                                                                                                  arestas_percorridas):
                     # verifico qual teve menor quantidade de visitas ...
-                    print("ambos foram visitados")
+                    # print("ambos foram visitados")
 
-                    if self.get_aresta_caminho(lista_aresta[n], arestas_percorridas).quantidade_visitas < self.get_aresta_caminho(melhor, arestas_percorridas).quantidade_visitas:
-                        # print("eae????")
-                    #if lista_aresta[n].quantidade_visitas < melhor.quantidade_visitas:
+                    if self.get_aresta_caminho(lista_aresta[n],
+                                               arestas_percorridas).quantidade_visitas < self.get_aresta_caminho(melhor,
+                                                                                                                 arestas_percorridas).quantidade_visitas:
+
                         melhor = lista_aresta[n]
-            # print(f'{melhor.destino} é o melhor vertice para prosseguir')
+
+                # print(f'{melhor.destino} é o melhor vertice para prosseguir')
+                if alternativo and len(self.caminhos) < 10:
+                    self.avanca_proximo(v, alternativo, len(self.caminhos) + 1)
+
+
             return melhor
+
         ######### FIM DA FUNCAO QUE VERIFICA A MELHOR ARESTA ###########
 
         # se tiver no começo da aplicação, ainda não temos caminhos, então vamos criar o primeiro
         if len(self.caminhos) == 0:
             # print("primeiro caminho")
             self.caminhos.append(Caminho(v))
-        else:
+        # else:
 
-         #   print(f'{ncaminho} {len(self.caminhos)}')
-         # se ja tiver caminho criado, vamos checar se é um novo caminho pelo ncaminho
-         # se o ncaminho for superior ao tamanho de caminhos, significa que estou querendo criar um novo caminho
-            if ncaminho > len(self.caminhos):
-                self.caminhos.append(deepcopy(self.caminhos[ncaminho - 2]))
-                self.caminhos[ncaminho - 1].percurso += v
-                self.caminhos[ncaminho - 1].visitado = True
+        #   print(f'{ncaminho} {len(self.caminhos)}')
+        # se ja tiver caminho criado, vamos checar se é um novo caminho pelo ncaminho
+        # se o ncaminho for superior ao tamanho de caminhos, significa que estou querendo criar um novo caminho
+        #    if ncaminho > len(self.caminhos):
+        #        self.caminhos.append(deepcopy(self.caminhos[ncaminho - 2]))
+        #        self.caminhos[ncaminho - 1].percurso += v
+        #        self.caminhos[ncaminho - 1].visitado = True
 
         # se nao terminou, ou seja, se ainda nao percorreu tudo e chegou ao inicial...
         # ncaminho < 10 eu coloquei para percorrer apenas 10 caminhos
-        if not self.terminou(v, self.caminhos[ncaminho - 1].arestas_percorridas) and ncaminho < 10:
+        if not self.terminou(v, self.caminhos[ncaminho - 1].arestas_percorridas):
 
             # se so tiver uma aresta para ir ate o vizinho do vertice atual, ja avança para o proximo
             if len(self.vertices[v].vizinhos) == 1:
@@ -255,6 +278,6 @@ class Grafo:
             # senao, vamos verificar qual aresta é melhor
             else:
                 # print("tem mais de um vizinho, vamos ver qual vai pegar")
-                self.avanca_proximo(v, melhor_aresta(self.vertices[v].vizinhos, self.caminhos[ncaminho - 1].arestas_percorridas,  ncaminho), ncaminho)
-                #self.avanca_proximo(v, melhor_aresta(self.vertices[v].vizinhos, ncaminho))
-
+                self.avanca_proximo(v, melhor_aresta(self.vertices[v].vizinhos,
+                                                     self.caminhos[ncaminho - 1].arestas_percorridas), ncaminho)
+                # self.avanca_proximo(v, melhor_aresta(self.vertices[v].vizinhos, ncaminho))
--- run.py
+++ run.py
@@ -1,25 +1,38 @@
 from grafo4 import Grafo, Vertice
-
+import sys
 import time
 import dados
 
+
+#print(sys.getrecursionlimit())
+#sys.setrecursionlimit(15000)
+
 g = Grafo()
+
+for i in range(13):
+    # print(str(i+1))
+    g.adiciona_vertice(Vertice(str(i+1)))
+
+for i in dados.nova_luz["arestas"]:
+    g.adiciona_aresta(i[0], i[1], i[2], i[3])
 
 #numero_grafo = int(input('Digite o número do grafo:'))
 numero_grafo = 3
 
-for i in dados.data[numero_grafo]["vertices"]:
-    #print(i)
-    g.adiciona_vertice(Vertice(i))
-
-for i in dados.data[numero_grafo]["arestas"]:
-    g.adiciona_aresta(i[0], i[1], i[2], i[3])
-
-#g.imprime_grafo()
-
-inicio = time.time()
-#g.encontra_caminho(input('digite o ponto de partida: ').upper())
-g.encontra_caminho('D')
-fim = time.time()
+# for i in dados.data[numero_grafo]["vertices"]:
+#     #print(i)
+#     g.adiciona_vertice(Vertice(i))
 #
-print('duracao: %f' % (fim - inicio))
+# for i in dados.data[numero_grafo]["arestas"]:
+#     g.adiciona_aresta(i[0], i[1], i[2], i[3])
+#
+#g.imprime_grafo()
+#
+# inicio = time.time()
+# #g.encontra_caminho(input('digite o ponto de partida: ').upper())
+g.encontra_caminho('1')
+
+
+# fim = time.time()
+# #
+# print('duracao: %f' % (fim - inicio))
